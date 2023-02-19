21cdb850c4e5a23a9aebc2d5f70f1551c6a17916
Caleb Akwesera Ndiwa <calebakwe@gmail.com>
2023-02-19 07:39:13 -0800

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,8 +17,9 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
-				/*eliminate repetitive digits*/
+				if (!((ones == tens)
+					|| (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+					/*eliminate repetitive digits*/
 				{
 					putchar(hundreds);
 					putchar(tens);
