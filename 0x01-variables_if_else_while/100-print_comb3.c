13a786b93a6481980a8bdc2ac0a68e482e3a019e
Caleb Akwesera Ndiwa <calebakwe@gmail.com>
2023-02-19 10:24:55 -0800

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,33 +3,32 @@
 /**
  * main- print all possible different combinations of two digits
  *
- * Return: Always 0 (Success)
- *
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
-
 {
-	int ones = '0';
-	int tens = '9';
+    int d1, d2;
+    int sep1 = 0;
+    int sep2 = 0;
 
-	for (tens = '0'; tens <= '9'; tens++)/*print tens digit*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
-		{
-			if (!((ones == tens) || (tens > ones)))/*removes repetition*/
-			{
-				putchar(',');
-				putchar(' ');
-				if (!(ones == '0' && '9'))/*adds comma and space*/
-				{
-					putchar(tens);
-					putchar(ones);
-				}
-			}
-		}
-		
-	}
-	putchar('\n');
-	return (0);
+    d1 = 48;
+    while (d1 < 58)
+    {
+        d2 = d1 + 1;
+        while (d2 < 58)
+        {
+            putchar(sep1);
+            putchar(sep2);
+            putchar(d1);
+            putchar(d2);
+            d2++;
+            sep1 = ',';
+            sep2 = ' ';
+        }
+        d1++;
+    }
+
+    putchar(10);
+    return (0);
 }
