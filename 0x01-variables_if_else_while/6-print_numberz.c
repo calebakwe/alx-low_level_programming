b949004a09292aa6c66a4e4675e28a584e390124
Caleb Akwesera Ndiwa <calebakwe@gmail.com>
2023-02-18 23:28:32 -0800

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
  * main- returns numbers of base 10
  *
@@ -10,13 +11,13 @@
 int main(void)
 
 {
-        int num = '0';
+	int num = '0';
 
-        while (num <= '9')
-        {
-                putchar(num);
-                num++;
-        }
-        putchar('\n');
-        return (0);
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
