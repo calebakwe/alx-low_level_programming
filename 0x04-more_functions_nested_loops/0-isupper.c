d5bfd14224e86497ea074866995bee3f3004d2c3
Caleb Akwesera Ndiwa <calebakwe@gmail.com>
2023-02-23 07:43:55 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
-/** main - check the code.
- *
+/** _isupper - check the code for uppercase.
+ *@x: The number to be checked
  * Return: Always 0.
  */
 int _isupper(int x)
 {
 	if (x >= 65 && x < 90)
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
